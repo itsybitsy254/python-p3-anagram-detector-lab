f392f7b4abe7f286f35b87d0ddf85619498b216a
Vitalis Ian <viterlis254@gmail.com>
2024-09-03 22:00:19 +0300

    initial

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        # Store the sorted version of the word for easy comparison
+        self.letters_in_word = sorted(word)
+
+    def match(self, word_list):
+        # Return a list of words from word_list that are anagrams of the initialized word
+        return [word for word in word_list if sorted(word) == self.letters_in_word]
